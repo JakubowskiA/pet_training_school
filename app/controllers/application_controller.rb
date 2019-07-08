5673a040b75af5f2ce46d007b482759dc7b2fe86
CindyChenx <chen_cx0128@hotmail.com>
2019-07-08 16:59:06 -0400

    add views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    def user 
+        if session[:user_id]
+            User.find(session[:user_id])
+        end
+    end
+
+    def logged_in?
+        user 
+    end 
+
+    def authorized?  
+        redirect_to login_path unless logged_in?
+    end
+
 end
