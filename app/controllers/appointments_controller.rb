c509858a5dd14daeabd4fe781dd16576cc5d307f
JakubowskiA <arieljakubowski@gmail.com>
2019-07-08 11:43:54 -0400

    Added forms for appointments, pets, and owners, added show pages for appointments and pets

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,28 @@
 class AppointmentsController < ApplicationController
   def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment.create(appointment_params)
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new
+    end
   end
 
   def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id]).destroy
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:pet_id, :lesson_id)
   end
 end
