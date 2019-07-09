006bdae27b3a5ce8bd393c2021c731235f76ae88
JakubowskiA <arieljakubowski@gmail.com>
2019-07-09 11:19:36 -0400

    Updated owner controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,34 @@
 class UsersController < ApplicationController
-    before_action :authorized?, except: [:new, :create]
+  before_action :authorized?, except: [:new, :create]
 
-    def new 
-        @user = User.new 
-    end
+  def new
+    @user = User.new
+  end
 
-    def create 
-        @user = User.create(user_params)
-        session[:user_id] = @user.id 
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
 
-        ####### need to decide redirect_to Owner or Trainer
-        
-    end
+    ####### need to decide redirect_to Owner or Trainer
 
-    private 
+  end
 
-    def user_params 
-        params.require(:user).permit(:name, :password, :username)
-    end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :username)
+  end
 end
