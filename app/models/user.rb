bd99e6f49f6be4ff441b216294506bf09266da3a
JakubowskiA <arieljakubowski@gmail.com>
2019-07-09 13:32:51 -0400

    Seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :username, presence: true
+  validates :username, uniqueness: true
   validates :password, presence: true
 end
