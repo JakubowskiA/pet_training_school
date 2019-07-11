e01f5ceaf15234d3de2977ab21c21ded71d59ae8
JakubowskiA <arieljakubowski@gmail.com>
2019-07-11 10:02:41 -0400

    updated CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    redirect_to user_path(session[:user_id])
   end
 
   def show
